decdef6adf291e19bb23ef7f6bdd59f45f9a1241
Playo245 <119519532+Playo245@users.noreply.github.com>
2022-12-09 13:02:04 +0000

    Stack imp

---
+++ stack_imp.py.py
@@ -0,0 +1,37 @@
+class Stack:
+    '''Python implementation the stack'''
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def top(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+## Sample inputs
+if __name__ == '__main__':
+    s = Stack()
+
+    print(s.is_empty())
+    s.push(4)
+    s.push('dog')
+    print(s.top())
+    s.push(True)
+    print(s.size())
+    print(s.is_empty())
+    s.push(8.4)
+    print(s.pop())
+    print(s.pop())
+    print(s.size())
